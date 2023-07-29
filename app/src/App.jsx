e81c7f969f5aae93dbab270eb67e36bfcfec81d3
Shantanu Sutar <shantanusutar17@gmail.com>
2023-07-29 15:01:45 +0530

    fetching data using dummyjson products api and displaying it

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,37 @@
 // https://dummyjson.com/products?limit=10
 
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>S</>;
+  const [products, setProducts] = useState([]);
+
+  async function fetchProducts() {
+    const res = await fetch(`https://dummyjson.com/products?limit=10`);
+    const data = await res.json();
+    if (data && data.products) setProducts(data.products);
+    console.log(products);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <div key={product.id} className="product">
+                <img src={product.thumbnail} alt="" />
+                <span>{product.title}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
